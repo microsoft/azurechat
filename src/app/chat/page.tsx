7582b95ae16c75eabc2c3be1869507b3ea9bc28e
giuliohome <giuliohome@users.noreply.github.com>
2023-09-27 08:59:45 +0000

    Fix #190 use Effect to fetch data

--- src/app/chat/page.tsx
+++ src/app/chat/page.tsx
@@ -1,14 +1,20 @@
+"use client";
+import { useEffect } from 'react';
 import { Card } from "@/components/ui/card";
 import { FindAllChatThreadForCurrentUser } from "@/features/chat/chat-services/chat-thread-service";
 import { StartNewChat } from "@/features/chat/chat-ui/start-new-chat";
 import { redirect } from "next/navigation";
 
-export default async function Home() {
-  const chats = await FindAllChatThreadForCurrentUser();
-  if (chats.length > 0) {
-    redirect(`/chat/${chats[0].id}`);
-  }
-
+export default function Home() {
+  useEffect(() => {
+    const fetchData = async () => {
+      const chats = await FindAllChatThreadForCurrentUser();
+      if (chats.length > 0) {
+        redirect(`/chat/${chats[0].id}`);
+      }
+    };
+    fetchData();
+  }, );
   return (
     <Card className="h-full items-center flex justify-center flex-1">
       <StartNewChat />
