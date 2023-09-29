1ac4995e82bf27083c1782dfbca56f74caab8cbd
thivy <thivyakanth@gmail.com>
2023-09-29 10:11:47 +1000

    fix image issue

--- src/features/chat/chat-ui/start-new-chat.tsx
+++ src/features/chat/chat-ui/start-new-chat.tsx
@@ -1,7 +1,6 @@
 import Typography from "@/components/typography";
 import { Card } from "@/components/ui/card";
 import { AI_NAME } from "@/features/theme/customise";
-import Image from "next/image";
 import { FC } from "react";
 import { NewChat } from "../chat-menu/new-chat";
 
@@ -11,7 +10,7 @@ export const StartNewChat: FC<Prop> = (props) => {
   return (
     <div className="grid grid-cols-5 w-full items-center container mx-auto max-w-3xl justify-center h-full gap-9">
       <div className="col-span-2 gap-5 flex flex-col flex-1">
-        <Image width={180} height={180} alt="" src="/ai-icon.png" />
+        <img src="/ai-icon.png" className="w-36" />
       </div>
       <Card className="col-span-3 flex flex-col gap-5 p-5 ">
         <Typography variant="h4" className="text-primary">
