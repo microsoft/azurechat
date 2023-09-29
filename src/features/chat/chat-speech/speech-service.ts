9632ecf97bb453b14c70df1fe0a4fcfbc377439d
thivy <thivyakanth@gmail.com>
2023-09-29 17:13:26 +1000

    add speech to text

---
+++ src/features/chat/chat-speech/speech-service.ts
@@ -0,0 +1,18 @@
+"use server";
+
+export const GetSpeechToken = async () => {
+  const response = await fetch(
+    `https://${process.env.AZURE_SPEECH_REGION}.api.cognitive.microsoft.com/sts/v1.0/issueToken`,
+    {
+      method: "POST",
+      headers: {
+        "Ocp-Apim-Subscription-Key": process.env.AZURE_SPEECH_KEY!,
+      },
+    }
+  );
+
+  return {
+    token: await response.text(),
+    region: process.env.AZURE_SPEECH_REGION,
+  };
+};
