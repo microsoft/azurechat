e5a50e37ca22cedaedc4767bb8f6dfe05a8e9d14
thivy <thivyakanth@gmail.com>
2023-09-29 10:56:57 +1000

    fix loading

--- src/features/loading-skeleton.tsx
+++ src/features/loading-skeleton.tsx
@@ -1,13 +1,11 @@
-import Image from "next/image";
 export const LoadingSkeleton = () => {
   return (
     <div className="h-full flex items-center justify-center bg-card flex-1">
-      <Image
+      <img
         width={80}
         height={80}
         alt=""
         src="/ai-icon.png"
-        quality={100}
         className="animate-bounce"
       />
     </div>
