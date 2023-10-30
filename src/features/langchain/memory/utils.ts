18a600ac2fd0c49507d31cc02b86c36cb4f70b19
thivy <thivyakanth@gmail.com>
2023-10-31 09:55:08 +1100

    refactor simple chat

--- src/features/langchain/memory/utils.ts
+++ src/features/langchain/memory/utils.ts
@@ -1,38 +1,13 @@
-import {
-  AIMessage,
-  BaseMessage,
-  ChatMessage,
-  ChatMessageFieldsWithRole,
-  HumanMessage,
-  StoredMessage,
-  SystemMessage,
-} from "langchain/schema";
+import { ChatMessageModel } from "@/features/chat/chat-services/models";
+import { ChatCompletionMessage } from "openai/resources";
 
-export function mapStoredMessagesToChatMessages(
-  messages: StoredMessage[]
-): BaseMessage[] {
+export function mapOpenAIChatMessages(
+  messages: ChatMessageModel[]
+): ChatCompletionMessage[] {
   return messages.map((message) => {
-    switch (message.data.role) {
-      case "human":
-        return new HumanMessage(message.data);
-      case "ai":
-        return new AIMessage(message.data);
-      case "system":
-        return new SystemMessage(message.data);
-      case "chat": {
-        if (message.data.role === undefined) {
-          // throw new Error("Role must be defined for chat messages");
-        }
-        return new ChatMessage(message.data as ChatMessageFieldsWithRole);
-      }
-      default:
-        throw new Error(`Got unexpected type: ${message.type}`);
-    }
+    return {
+      role: message.role,
+      content: message.content,
+    };
   });
-}
-
-export function mapChatMessagesToStoredMessages(
-  messages: BaseMessage[]
-): StoredMessage[] {
-  return messages.map((message) => message.toDict());
 }
