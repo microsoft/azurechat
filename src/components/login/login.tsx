3cebf224ef4d6ae76f63ef18e9a3dda2a4a7b126
Cédric Menzi <cedric.menzi@buhlergroup.com>
2023-11-02 11:35:13 +0100

    fix

--- src/components/login/login.tsx
+++ src/components/login/login.tsx
@@ -22,12 +22,12 @@ export const LogIn = () => {
           <span className="text-primary">{AI_NAME}</span>
         </CardTitle>
         <CardDescription>
-          Login in with your GitHub or Microsoft 365 account
+          Login in with your Microsoft Entra ID account
         </CardDescription>
       </CardHeader>
       <CardContent className="grid gap-4">
-        <Button onClick={() => signIn("github")}>GitHub</Button>
-        <Button onClick={() => signIn("azure-ad")}> Microsoft 365</Button>
+        {/* <Button onClick={() => signIn("github")}>GitHub</Button> */}
+        <Button onClick={() => signIn("azure-ad")}>Microsoft Entra ID</Button>
         {process.env.NODE_ENV === "development" && (
           <Button onClick={() => signIn("localdev")}>Basic Auth (DEV ONLY)</Button>
         )}
