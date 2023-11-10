ce1103dc6905a128ba0cd6b82e54de9531d889f6
Eric Suiter <eric.suiter@wustl.edu>
2023-11-10 15:24:27 -0600

    Update login.tsx

--- src/components/login/login.tsx
+++ src/components/login/login.tsx
@@ -22,12 +22,11 @@ export const LogIn = () => {
           <span className="text-primary">{AI_NAME}</span>
         </CardTitle>
         <CardDescription>
-          Login in with your GitHub or Microsoft 365 account
+          Login in with your WUSTL KEY
         </CardDescription>
       </CardHeader>
       <CardContent className="grid gap-4">
-        <Button onClick={() => signIn("github")}>GitHub</Button>
-        <Button onClick={() => signIn("azure-ad")}> Microsoft 365</Button>
+        <Button onClick={() => signIn("azure-ad")}>WUSTL Login</Button>
         {process.env.NODE_ENV === "development" && (
           <Button onClick={() => signIn("localdev")}>Basic Auth (DEV ONLY)</Button>
         )}
