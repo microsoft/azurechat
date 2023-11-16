5138d82e126cb59072a1f70ef5c211e938be12fa
Eric Suiter <ericsuiter@gmail.com>
2023-11-16 16:53:24 -0600

    Update openai.ts

--- src/features/common/openai.ts
+++ src/features/common/openai.ts
@@ -4,11 +4,11 @@ export const OpenAIInstance = () => {
   const openai = new OpenAI({
     apiKey: process.env.OPENAI_API_KEY,
     baseURL: `https://${process.env.AZURE_OPENAI_API_ENDPOINT}/deployments/${process.env.AZURE_OPENAI_API_DEPLOYMENT_NAME}`,
+    defaultQuery: { "api-version": process.env.AZURE_OPENAI_API_VERSION },
     defaultHeaders: { 
       "api-key": process.env.OPENAI_API_KEY, 
       "deployment-id": process.env.AZURE_OPENAI_API_DEPLOYMENT_NAME,
-      "Ocp-Apim-Subscription-Key": process.env.OCP_APIM_SUBSCRIPTION_KEY,
-      "api-version": process.env.AZURE_OPENAI_API_VERSION
+      "Ocp-Apim-Subscription-Key": process.env.OCP_APIM_SUBSCRIPTION_KEY
     },
   });
   return openai;
@@ -18,11 +18,11 @@ export const OpenAIEmbeddingInstance = () => {
   const openai = new OpenAI({
     apiKey: process.env.OPENAI_API_KEY,
     baseURL: `https://${process.env.AZURE_OPENAI_API_ENDPOINT}/deployments/${process.env.AZURE_OPENAI_API_EMBEDDINGS_DEPLOYMENT_NAME}`,
+    defaultQuery: { "api-version": process.env.AZURE_OPENAI_API_VERSION },
     defaultHeaders: { 
       "api-key": process.env.OPENAI_API_KEY, 
       "deployment-id": process.env.AZURE_OPENAI_API_EMBEDDINGS_DEPLOYMENT_NAME,
-      "Ocp-Apim-Subscription-Key": process.env.OCP_APIM_SUBSCRIPTION_KEY,
-      "api-version": process.env.AZURE_OPENAI_API_VERSION
+      "Ocp-Apim-Subscription-Key": process.env.OCP_APIM_SUBSCRIPTION_KEY
     },
   });
   return openai;
