7e392f284bd2acc62179947b1e29159c8ecbe100
Matt Weston <MattOfNZ@users.noreply.github.com>
2023-12-02 13:39:38 +1300

    Boilerplate for elearning

---
+++ src/app/learn/page.tsx
@@ -0,0 +1,11 @@
+import { Card } from "@/components/ui/card";
+
+export const dynamic = "force-dynamic";
+
+export default async function Home() {
+  return (
+    <Card className="h-full items-center flex justify-center flex-1">
+      learning
+    </Card>
+  );
+}
---
+++ src/components/learn/learn-data.tsx
@@ -0,0 +1,184 @@
+export default [
+  {
+    slug: "module-1",
+    name: "Module 1",
+    content: <>Module one content goes here</>,
+    questions: [
+      {
+        question: "What is the capital of France?",
+        options: [
+          {
+            option: "Paris",
+            correct: true,
+          },
+          {
+            option: "London",
+            correct: false,
+          },
+          {
+            option: "New York",
+            correct: false,
+          },
+          {
+            option: "Dubai",
+            correct: false,
+          },
+        ],
+      },
+      {
+        question: "What is the capital of India?",
+        options: [
+          {
+            option: "Paris",
+            correct: false,
+          },
+          {
+            option: "London",
+            correct: false,
+          },
+          {
+            option: "New York",
+            correct: false,
+          },
+          {
+            option: "Delhi",
+            correct: true,
+          },
+        ],
+      },
+      {
+        question: "What is the capital of USA?",
+        options: [
+          {
+            option: "Paris",
+            correct: false,
+          },
+          {
+            option: "Washington",
+            correct: true,
+          },
+          {
+            option: "New York",
+            correct: false,
+          },
+          {
+            option: "Dubai",
+            correct: false,
+          },
+        ],
+      },
+      {
+        question: "What is the capital of UAE?",
+        options: [
+          {
+            option: "Paris",
+            correct: false,
+          },
+          {
+            option: "London",
+            correct: false,
+          },
+          {
+            option: "New York",
+            correct: false,
+          },
+          {
+            option: "Dubai",
+            correct: true,
+          },
+        ],
+      },
+    ],
+  },
+  {
+    slug: "module-2",
+    name: "Module 2",
+    content: <>Module two content goes here</>,
+    questions: [
+      {
+        question: "What is the capital of France?",
+        options: [
+          {
+            option: "Paris",
+            correct: true,
+          },
+          {
+            option: "London",
+            correct: false,
+          },
+          {
+            option: "New York",
+            correct: false,
+          },
+          {
+            option: "Dubai",
+            correct: false,
+          },
+        ],
+      },
+      {
+        question: "What is the capital of India?",
+        options: [
+          {
+            option: "Paris",
+            correct: false,
+          },
+          {
+            option: "London",
+            correct: false,
+          },
+          {
+            option: "New York",
+            correct: false,
+          },
+          {
+            option: "Delhi",
+            correct: true,
+          },
+        ],
+      },
+      {
+        question: "What is the capital of USA?",
+        options: [
+          {
+            option: "Paris",
+            correct: false,
+          },
+          {
+            option: "Washington",
+            correct: true,
+          },
+          {
+            option: "New York",
+            correct: false,
+          },
+          {
+            option: "Dubai",
+            correct: false,
+          },
+        ],
+      },
+      {
+        question: "What is the capital of UAE?",
+        options: [
+          {
+            option: "Paris",
+            correct: false,
+          },
+          {
+            option: "London",
+            correct: false,
+          },
+          {
+            option: "New York",
+            correct: false,
+          },
+          {
+            option: "Dubai",
+            correct: true,
+          },
+        ],
+      },
+    ],
+  },
+];
