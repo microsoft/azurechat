073a4a5f54beadbab806a0cceceaab4c0d787ea3
AzureAD\AmirKamel <amir.kamel@qdap.qld.gov.au>
2023-12-11 14:22:01 +1000

    Added session update

--- src/features/chat/chat-menu/new-chat.tsx
+++ src/features/chat/chat-menu/new-chat.tsx
@@ -4,8 +4,35 @@ import { Button } from "@/components/ui/button";
 import { PlusCircle } from "lucide-react";
 import { useRouter } from "next/navigation";
 import { CreateChatThread } from "../chat-services/chat-thread-service";
+import { useEffect } from "react";
+import { useSession } from "next-auth/react";
 
-export const NewChat = () => {
+
+export const NewChat = async () => {
+
+  const { data: session, status, update } = useSession()
+
+  // Polling the session every 1 hour
+  useEffect(() => {
+
+  //   //set 1 hour interval for refetching the session
+    const interval = setInterval(() => update(), 1000 * 60 * 60)
+    // console.log("current session: ");
+    return () => clearInterval(interval)
+  }, [update])
+
+  // Listen for when the page is visible, if the user switches tabs
+  // and makes our tab visible again, re-fetch the session
+  useEffect(() => {
+    const visibilityHandler = () =>
+      document.visibilityState === "visible" && update()
+    window.addEventListener("visibilitychange", visibilityHandler, false)
+    return () =>
+      window.removeEventListener("visibilitychange", visibilityHandler, false)
+  }, [update])
+
+
+
   const router = useRouter();
   const startNewChat = async () => {
     try {
