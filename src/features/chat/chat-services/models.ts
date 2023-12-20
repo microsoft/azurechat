5ca16c7092ce36bf2836011bad02075b9e167d8c
ncigitgpt <154388104+ncigitgpt@users.noreply.github.com>
2023-12-20 10:22:54 +0900

    Update models.ts

--- src/features/chat/chat-services/models.ts
+++ src/features/chat/chat-services/models.ts
@@ -16,7 +16,7 @@ export interface ChatMessageModel {
   type: "CHAT_MESSAGE";
 }
 
-export type ConversationStyle = "creative" | "balanced" | "precise";
+export type ConversationStyle = "創造的" | "バランス" | "厳格";
 export type ChatType = "simple" | "data" | "mssql";
 
 export type ChatRole = "system" | "user" | "assistant" | "function";
