92c3b7843fde10217d70a4f1603dd4e6af6a0651
AzureAD\EllaSalehi <ella.salehi@chde.qld.gov.au>
2024-01-09 12:01:59 +1000

    no message

--- src/features/chat/chat-ui/chat-ui.tsx
+++ src/features/chat/chat-ui/chat-ui.tsx
@@ -6,20 +6,25 @@ import { ChatMessageEmptyState } from "./chat-empty-state/chat-message-empty-sta
 import ChatInput from "./chat-input/chat-input";
 import { ChatMessageContainer } from "./chat-message-container";
 
-interface Prop {}
+interface Prop {
+  chatId: string;
+  onPromptSelected: () => string;
+};
 
-export const ChatUI: FC<Prop> = () => {
+export const ChatUI: FC<Prop> = ({ chatId, onPromptSelected }) => {
   const { messages } = useChatContext();
 
   return (
     <div className="h-full relative overflow-hidden flex-1 bg-card rounded-md shadow-md">
-      {messages.length !== 0 ? (
-        <ChatMessageContainer />
-      ) : (
-        <ChatMessageEmptyState />
-      )}
+    {messages.length !== 0 ? (
+      <ChatMessageContainer/>
+    ) : (
+      <ChatMessageEmptyState onPromptSelected={onPromptSelected} />
+    )}
 
-      <ChatInput />
-    </div>
+    <ChatInput />
+
+  </div>
+
   );
 };
