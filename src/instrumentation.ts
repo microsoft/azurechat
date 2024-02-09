fca355063a302c983751f7ffabc6e7734985100f
samuel.ochsner <samuel.ochsner@buhlergroup.com>
2024-02-09 23:08:22 +0100

    Update instrumentation and next.config.js

--- src/instrumentation.ts
+++ src/instrumentation.ts
@@ -1,6 +1,19 @@
-export async function register() {
+export function register() {
   if (process.env.NEXT_RUNTIME === 'nodejs') {
     console.log('Node.js instrumentation');
-    await import('./instrumentation.node');
+    const { useAzureMonitor } = require('@azure/monitor-opentelemetry');
+    const { metrics } = require('@opentelemetry/api');
+
+    //process.env.APPLICATIONINSIGHTS_INSTRUMENTATION_LOGGING_LEVEL = "VERBOSE";
+    //process.env.APPLICATIONINSIGHTS_LOG_DESTINATION = "file+console";
+    useAzureMonitor({
+        azureMonitorExporterOptions: {
+          connectionString: process.env.APPLICATIONINSIGHTS_CONNECTION_STRING || "",
+        }
+      });
+
+    console.log(metrics.getMeterProvider());
+
+    console.log("Application Insights Connection String: ", process.env.APPLICATIONINSIGHTS_CONNECTION_STRING)
   }
 }
--- src/next.config.js
+++ src/next.config.js
@@ -1,7 +1,6 @@
 /** @type {import('next').NextConfig} */
 const nextConfig = {
   output: "standalone",
-  swcMinify: false,
   experimental: {
     serverComponentsExternalPackages: ["@azure/storage-blob"],
   },
