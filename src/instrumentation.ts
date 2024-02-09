cfc61bdd84eff9660f3804ae8d4129a1197a851c
samuel.ochsner <samuel.ochsner@buhlergroup.com>
2024-02-09 16:33:05 +0100

    Refactor instrumentation code to use OpenTelemetry metrics

--- src/instrumentation.ts
+++ src/instrumentation.ts
@@ -2,6 +2,21 @@ export async function register() {
   // eslint-disable-next-line react-hooks/rules-of-hooks
 
   if (process.env.NEXT_RUNTIME === 'nodejs') {
-      await import('./instrumentation.node');
+    const { metrics } = require("@opentelemetry/api");
+    const { MeterProvider, PeriodicExportingMetricReader } = require("@opentelemetry/sdk-metrics");
+    const { AzureMonitorMetricExporter } = require("@azure/monitor-opentelemetry-exporter");
+    const exporter = new AzureMonitorMetricExporter({
+        connectionString: process.env.APPLICATIONINSIGHTS_CONNECTION_STRING || "",
+    });
+    const metricReaderOptions = {
+        exporter: exporter,
+    };
+    const metricReader = new PeriodicExportingMetricReader(metricReaderOptions);
+    const meterProvider = new MeterProvider({
+        readers: [metricReader]
+    });
+    
+    // Register Meter Provider as global
+    metrics.setGlobalMeterProvider(meterProvider);
   }
 }
