f23cb1aff43acf38b9c37ca1c9ee8be2104e15e0
Josh Savachik <josh.savachik@apigroupinc.us>
2024-02-13 16:20:31 -0500

    Updating theme and AI name to match our branding

--- src/features/theme/theme-config.ts
+++ src/features/theme/theme-config.ts
@@ -1,5 +1,5 @@
-export const AI_NAME = "Azure Chat";
-export const AI_DESCRIPTION = "Azure Chat is a friendly AI assistant.";
+export const AI_NAME = "APiChat";
+export const AI_DESCRIPTION = "APiChat is a friendly AI assistant designed to assist employees of APi Group Inc.";
 export const CHAT_DEFAULT_PERSONA = AI_NAME + " default";
 
 export const CHAT_DEFAULT_SYSTEM_PROMPT = `You are a friendly ${AI_NAME} AI assistant. You must always return in markdown format.
