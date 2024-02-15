9de1534a6c2672f387abd5995c92adbf091f5c75
Jason Leong <jasonleong@microsoft.com>
2024-02-15 13:36:17 +1100

    Added ARIA roles and labels

--- src/features/ui/chat/chat-input-area/submit-chat.tsx
+++ src/features/ui/chat/chat-input-area/submit-chat.tsx
@@ -6,7 +6,7 @@ export const SubmitChat = React.forwardRef<
   HTMLButtonElement,
   React.HTMLAttributes<HTMLButtonElement> // Add ChatInputAreaProps to the type definition
 >(({ ...props }, ref) => (
-  <Button size="icon" type="submit" variant={"ghost"} {...props} ref={ref}>
+  <Button size="icon" type="submit" variant={"ghost"} {...props} ref={ref} aria-label="Submit chat input">
     <Send size={16} />
   </Button>
 ));
