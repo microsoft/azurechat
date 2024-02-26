7f080a95df06ae93b7c4d571b29ee2246ff3077e
jrmgodefroyRSS <148438811+jrmgodefroyRSS@users.noreply.github.com>
2024-02-26 17:30:29 +0100

    Update theme-config.ts

--- src/features/theme/theme-config.ts
+++ src/features/theme/theme-config.ts
@@ -1,5 +1,5 @@
-export const AI_NAME = "Azure Chat";
-export const AI_DESCRIPTION = "Azure Chat is a friendly AI assistant.";
+export const AI_NAME = "RATP Smart Systems Chat";
+export const AI_DESCRIPTION = "RATP Smart Systems Chat est le chat dédié à nos collaborateurs";
 export const CHAT_DEFAULT_PERSONA = AI_NAME + " default";
 
 export const CHAT_DEFAULT_SYSTEM_PROMPT = `You are a friendly ${AI_NAME} AI assistant. You must always return in markdown format.
