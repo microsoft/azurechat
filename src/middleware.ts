e67570af31b5706958a60d7b9dfa376fa7b488e8
Keith Oak <keith.oak@chde.qld.gov.au>
2024-03-27 12:38:25 +1000

    Fix middleware import statements

--- src/middleware.ts
+++ src/middleware.ts
@@ -1,8 +1,8 @@
-import { getToken } from "next-auth/jwt";
-import { NextRequest, NextResponse } from "next/server";
+import { getToken } from "next-auth/jwt"
+import { NextRequest, NextResponse } from "next/server"
 
-const LOGIN_PAGE = "/login";
-const UNAUTHORISED_PAGE = "/unauthorised";
+const LOGIN_PAGE = "/login"
+const UNAUTHORISED_PAGE = "/unauthorised"
 
 const requireAuth: string[] = [
   "/admin",
@@ -17,38 +17,35 @@ const requireAuth: string[] = [
   "/tenant",
   "/terms",
   "/whats-new",
-];
+]
 
-const requireAdmin: string[] = ["/admin", "/reporting", "/settings", "/tenant"];
+const requireAdmin: string[] = ["/admin", "/reporting", "/settings", "/tenant"]
 
 export async function middleware(request: NextRequest): Promise<NextResponse> {
   if (process.env.NODE_ENV === "development") {
-    return NextResponse.next();
+    return NextResponse.next()
   }
 
-  const pathname = request.nextUrl.pathname;
+  const pathname = request.nextUrl.pathname
 
-  if (requireAuth.some((path) => pathname.startsWith(path))) {
-    const token = await getToken({ req: request });
+  if (requireAuth.some(path => pathname.startsWith(path))) {
+    const token = await getToken({ req: request })
 
     if (!token) {
-      return NextResponse.redirect(new URL(LOGIN_PAGE, request.url));
+      return NextResponse.redirect(new URL(LOGIN_PAGE, request.url))
     }
 
-    const now = Math.floor(Date.now() / 1000);
+    const now = Math.floor(Date.now() / 1000)
     if (token.exp && typeof token.exp === "number" && token.exp < now) {
-      return NextResponse.redirect(new URL(LOGIN_PAGE, request.url));
+      return NextResponse.redirect(new URL(LOGIN_PAGE, request.url))
     }
 
-    if (
-      requireAdmin.some((path) => pathname.startsWith(path)) &&
-      !token.qchatAdmin
-    ) {
-      return NextResponse.rewrite(new URL(UNAUTHORISED_PAGE, request.url));
+    if (requireAdmin.some(path => pathname.startsWith(path)) && !token.qchatAdmin) {
+      return NextResponse.rewrite(new URL(UNAUTHORISED_PAGE, request.url))
     }
   }
 
-  return NextResponse.next();
+  return NextResponse.next()
 }
 
 export const config = {
@@ -68,4 +65,4 @@ export const config = {
     "/unauthorised/:path*",
     "/whats-new/:path*",
   ],
-};
+}
