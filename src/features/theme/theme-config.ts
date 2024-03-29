bd5fa45860b9b4e734d28b860433cc3409758b54
Josh Savachik <josh.savachik@apigroupinc.us>
2024-03-29 08:50:06 -0500

    Updated wording in AI description to match APi brand language

--- src/features/theme/theme-config.ts
+++ src/features/theme/theme-config.ts
@@ -1,5 +1,5 @@
 export const AI_NAME = "APiChat";
-export const AI_DESCRIPTION = "APiChat is a friendly AI assistant designed to assist employees of APi Group Inc.";
+export const AI_DESCRIPTION = "APiChat is a friendly AI assistant designed to assist team members of APi Group Inc.";
 export const CHAT_DEFAULT_PERSONA = AI_NAME + " default";
 
 export const CHAT_DEFAULT_SYSTEM_PROMPT = `You are a friendly ${AI_NAME} AI assistant. You must always return in markdown format.
