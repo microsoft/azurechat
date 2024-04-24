d87aaa58837a1a213b5b49c74e5b8c7102363f9c
Mingus <megamingus@gmail.com>
2024-04-24 19:31:56 +0200

    Change name to Mural’s Chatbot

--- src/features/theme/theme-config.ts
+++ src/features/theme/theme-config.ts
@@ -1,5 +1,5 @@
-export const AI_NAME = "Azure Chat";
-export const AI_DESCRIPTION = "Azure Chat is a friendly AI assistant.";
+export const AI_NAME = "Mural's Chatbot";
+export const AI_DESCRIPTION = "Mural's Chatbot is a friendly AI assistant.";
 export const CHAT_DEFAULT_PERSONA = AI_NAME + " default";
 
 export const CHAT_DEFAULT_SYSTEM_PROMPT = `You are a friendly ${AI_NAME} AI assistant. You must always return in markdown format.
