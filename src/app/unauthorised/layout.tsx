6bf9093ba460af3033fc47eb1695c0fd7790fb41
Keith Oak <150318763+kpqdap@users.noreply.github.com>
2024-05-12 07:12:04 +1000

    Fix typo in page title for Unauthorised (#65)
    Correct Page Title for Unauthorised

--- src/app/unauthorised/layout.tsx
+++ src/app/unauthorised/layout.tsx
@@ -1,8 +1,8 @@
 import { AI_NAME } from "@/features/theme/theme-config"
 
 export const metadata = {
-  title: AI_NAME + " Unathorised",
-  description: AI_NAME + " - Unathorised",
+  title: AI_NAME + " Unauthorised",
+  description: AI_NAME + " - Unauthorised",
 }
 
 export default function RootLayout({ children }: { children: React.ReactNode }): JSX.Element {
