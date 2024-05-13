e4fe997acd8c06eb6107b5422a17f63a569ee1ee
adamlimh <adamlimh@gmail.com>
2024-05-13 15:39:05 +0800

    Update login.tsx

--- src/features/auth-page/login.tsx
+++ src/features/auth-page/login.tsx
@@ -27,12 +27,11 @@ export const LogIn: FC<LoginProps> = (props) => {
           <span className="text-primary">{AI_NAME}</span>
         </CardTitle>
         <CardDescription>
-          Login in with your GitHub or Microsoft 365 account
+          Login in with your Microsoft account
         </CardDescription>
       </CardHeader>
       <CardContent className="grid gap-4">
-        <Button onClick={() => signIn("github")}>GitHub</Button>
-        <Button onClick={() => signIn("azure-ad")}> Microsoft 365</Button>
+        <Button onClick={() => signIn("azure-ad")}> Microsoft</Button>
         {props.isDevMode ? (
           <Button onClick={() => signIn("localdev")}>
             Basic Auth (DEV ONLY)
