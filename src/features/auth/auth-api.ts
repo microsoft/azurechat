bce14cc719a6930b2c622bb807e830b1b2e2266e
Keith Oak <150318763+kpqdap@users.noreply.github.com>
2024-05-27 16:05:30 +1000

    Update auth-api.ts scope for Azure AD authentication (#83)
    chore: Update auth-api.ts scope for Azure AD authentication

--- src/features/auth/auth-api.ts
+++ src/features/auth/auth-api.ts
@@ -33,7 +33,7 @@ const configureIdentityProvider = (): Provider[] => {
             client_id: process.env.AZURE_AD_CLIENT_ID,
             redirect_uri: process.env.NEXTAUTH_URL + "/api/auth/callback/azure-ad",
             response_type: "code",
-            scope: "openid profile email User.Read offline_access",
+            scope: "openid profile email User.Read",
           },
         },
         token: {
