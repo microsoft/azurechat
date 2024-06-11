d86a2aa79529d89bd4843953897a43d546d98a6a
Sandeep P S <sandymankara11@gmail.com>
2024-06-11 09:56:30 +0530

    style: update chat area button and text colors

--- src/features/ui/chat/chat-input-area/submit-chat.tsx
+++ src/features/ui/chat/chat-input-area/submit-chat.tsx
@@ -1,4 +1,4 @@
-import { Send } from "lucide-react";
+import { Forward } from "lucide-react";
 import React from "react";
 import { Button } from "../../button";
 
@@ -6,8 +6,14 @@ export const SubmitChat = React.forwardRef<
   HTMLButtonElement,
   React.HTMLAttributes<HTMLButtonElement> // Add ChatInputAreaProps to the type definition
 >(({ ...props }, ref) => (
-  <Button size="icon" type="submit" variant={"ghost"} {...props} ref={ref} aria-label="Submit chat input">
-    <Send size={16} />
+  <Button
+    size="icon"
+    type="submit"
+    {...props}
+    ref={ref}
+    aria-label="Submit chat input"
+  >
+    <Forward size={16} className="text-white" />
   </Button>
 ));
 SubmitChat.displayName = "ChatInputArea";
