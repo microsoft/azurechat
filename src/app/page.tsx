754b4c0b5e647355675122af0d684ae81aeb5943
samuel.ochsner <samuel.ochsner@buhlergroup.com>
2024-06-15 21:55:04 +0200

    chore: Move redirection logic into middleware

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,8 +1,6 @@
-import { redirectIfAuthenticated } from "@/features/auth-page/helpers";
 import { LogIn } from "@/features/auth-page/login";
 
 export default async function Home() {
-  await redirectIfAuthenticated();
   return (
     <main className="container max-w-lg flex items-center">
       <LogIn isDevMode={process.env.NODE_ENV === "development"} />
--- src/middleware.ts
+++ src/middleware.ts
@@ -14,20 +14,32 @@ const requireAdmin: string[] = ["/reporting"];
 export async function middleware(request: NextRequest) {
   const res = NextResponse.next();
   const pathname = request.nextUrl.pathname;
+  // Check if the user is trying to access the root path
+  if (pathname === '/') {
+    const token = await getToken({
+      req: request,
+    });
+
+    // If the user is logged in, redirect to /chat
+    if (token) {
+      const url = new URL(`/chat`, request.url);
+      return NextResponse.redirect(url);
+    }
+  }
 
   if (requireAuth.some((path) => pathname.startsWith(path))) {
     const token = await getToken({
       req: request,
     });
 
-    //check not logged in
+    // Check not logged in
     if (!token) {
       const url = new URL(`/`, request.url);
       return NextResponse.redirect(url);
     }
 
     if (requireAdmin.some((path) => pathname.startsWith(path))) {
-      //check if not authorized
+      // Check if not authorized
       if (!token.isAdmin) {
         const url = new URL(`/unauthorized`, request.url);
         return NextResponse.rewrite(url);
@@ -41,6 +53,7 @@ export async function middleware(request: NextRequest) {
 // note that middleware is not applied to api/auth as this is required to logon (i.e. requires anon access)
 export const config = {
   matcher: [
+    "/",
     "/unauthorized/:path*",
     "/reporting/:path*",
     "/api/chat:path*",
