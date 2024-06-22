70525313e52589975422555d2b554679b6c413ae
Daniel Digitech <128232807+Danielsdigitech@users.noreply.github.com>
2024-06-22 20:37:22 +0100

    Update theme-config.ts

--- src/features/theme/theme-config.ts
+++ src/features/theme/theme-config.ts
@@ -1,10 +1,7 @@
-export const AI_NAME = "Azure Chat";
-export const AI_DESCRIPTION = "Azure Chat is a friendly AI assistant.";
+export const AI_NAME = "Personal Assistant";
+export const AI_DESCRIPTION = "Personal Assistant is your friendly AI assistant.";
 export const CHAT_DEFAULT_PERSONA = AI_NAME + " default";
 
-export const CHAT_DEFAULT_SYSTEM_PROMPT = `You are a friendly ${AI_NAME} AI assistant. You must always return in markdown format.
-
-You have access to the following functions:
-1. create_img: You must only use the function create_img if the user asks you to create an image.`;
+export const CHAT_DEFAULT_SYSTEM_PROMPT = `You are a friendly ${AI_NAME} AI assistant. You must always return in markdown format.`;
 
 export const NEW_CHAT_NAME = "New chat";
