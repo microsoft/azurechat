699bb62bfca252125842673c238ab8406dc0938c
Daniel Digitech <128232807+Danielsdigitech@users.noreply.github.com>
2024-06-26 23:28:19 +0100

    Update auth-api.ts

--- src/features/auth-page/auth-api.ts
+++ src/features/auth-page/auth-api.ts
@@ -1,16 +1,55 @@
-import NextAuth, { NextAuthOptions } from "next-auth";
+import { createHash } from "crypto";
+import { getServerSession } from "next-auth";
+import { RedirectToPage } from "../common/navigation-helpers";
+import { options } from "./auth-api";
 
-const configureIdentityProvider = () => {
-  const providers = []; // No providers needed for unauthenticated access
-  return providers;
+export const userSession = async (): Promise<UserModel | null> => {
+  const session = await getServerSession(options);
+  if (session && session.user) {
+    return {
+      name: session.user.name!,
+      image: session.user.image!,
+      email: session.user.email!,
+      isAdmin: session.user.isAdmin!,
+    };
+  }
+
+  return null;
 };
 
-export const options: NextAuthOptions = {
-  secret: process.env.NEXTAUTH_SECRET,
-  providers: [...configureIdentityProvider()],
-  session: {
-    strategy: "jwt",
-  },
+export const getCurrentUser = async (): Promise<UserModel> => {
+  const user = await userSession();
+  if (user) {
+    return user;
+  }
+  throw new Error("User not found");
 };
 
-export const handlers = NextAuth(options);
+export const userHashedId = async (): Promise<string> => {
+  const user = await userSession();
+  if (user) {
+    return hashValue(user.email);
+  }
+
+  throw new Error("User not found");
+};
+
+export const hashValue = (value: string): string => {
+  const hash = createHash("sha256");
+  hash.update(value);
+  return hash.digest("hex");
+};
+
+export const redirectIfAuthenticated = async () => {
+  const user = await userSession();
+  if (user) {
+    RedirectToPage("chat");
+  }
+};
+
+export type UserModel = {
+  name: string;
+  image: string;
+  email: string;
+  isAdmin: boolean;
+};
