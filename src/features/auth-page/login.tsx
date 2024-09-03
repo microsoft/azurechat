74008241f6165ad7a093b7b0ef2b4bd7b3686af6
Jason McReynolds <jmcreyn@wested.org>
2024-09-02 21:31:41 -0700

    removed GitHub login button

--- src/features/auth-page/login.tsx
+++ src/features/auth-page/login.tsx
@@ -31,7 +31,7 @@ export const LogIn: FC<LoginProps> = (props) => {
         </CardDescription>
       </CardHeader>
       <CardContent className="grid gap-4">
-        <Button onClick={() => signIn("github")}>GitHub</Button>
+        {/* <Button onClick={() => signIn("github")}>GitHub</Button>--> */}
         <Button onClick={() => signIn("azure-ad")}> Microsoft 365</Button>
         {props.isDevMode ? (
           <Button onClick={() => signIn("localdev")}>
