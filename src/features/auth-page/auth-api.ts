d02f3c8db0b06ad7eb35bc82bc7f9b21b974647c
Jason McReynolds <jmcreyn@wested.org>
2024-09-02 21:10:34 -0700

    missing email address fix for auth api

--- src/features/auth-page/auth-api.ts
+++ src/features/auth-page/auth-api.ts
@@ -44,9 +44,8 @@ const configureIdentityProvider = () => {
             ...profile,
             // throws error without this - unsure of the root cause (https://stackoverflow.com/questions/76244244/profile-id-is-missing-in-google-oauth-profile-response-nextauth)
             id: profile.sub,
-            isAdmin:
-              adminEmails?.includes(profile.email.toLowerCase()) ||
-              adminEmails?.includes(profile.preferred_username.toLowerCase()),
+            email: profile.preferred_username,
+            isAdmin: adminEmails?.includes(profile.email.toLowerCase()) || adminEmails?.includes(profile.preferred_username.toLowerCase()),
           };
           return newProfile;
         },
@@ -109,25 +108,7 @@ const configureIdentityProvider = () => {
         },
       })
     );
-  } else {
-    AzureADProvider({
-      clientId: process.env.AZURE_AD_CLIENT_ID!,
-      clientSecret: process.env.AZURE_AD_CLIENT_SECRET!,
-      tenantId: process.env.AZURE_AD_TENANT_ID!,
-      async profile(profile) {
-        const newProfile = {
-          ...profile,
-          // throws error without this - unsure of the root cause (https://stackoverflow.com/questions/76244244/profile-id-is-missing-in-google-oauth-profile-response-nextauth)
-          id: profile.sub,
-          isAdmin:
-            adminEmails?.includes(profile.email.toLowerCase()) ||
-            adminEmails?.includes(profile.preferred_username.toLowerCase()),
-        };
-        return newProfile;
-      },
-    })
   }
-
 
   return providers;
 };
