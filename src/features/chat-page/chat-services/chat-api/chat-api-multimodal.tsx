8c3f84c799195e5b94555e14b4cbe4b4ce43c125
ROZ a.k.a. Aoi Emerauda <35634920+ROZ-MOFUMOFU-ME@users.noreply.github.com>
2024-09-22 22:43:46 +0900

    Update chat-api-multimodal.tsx

--- src/features/chat-page/chat-services/chat-api/chat-api-multimodal.tsx
+++ src/features/chat-page/chat-services/chat-api/chat-api-multimodal.tsx
@@ -18,7 +18,7 @@ export const ChatApiMultimodal = (props: {
     {
       model: "",
       stream: true,
-      max_tokens: 4096,
+      max_tokens: 16384,
       messages: [
         {
           role: "system",
