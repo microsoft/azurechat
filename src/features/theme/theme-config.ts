336f5e543fee80ff0da64358d4f53cec572051b7
saidi59 <afnansayeed59@gmail.com>
2024-10-10 01:55:28 +0600

    Update theme-config.ts

--- src/features/theme/theme-config.ts
+++ src/features/theme/theme-config.ts
@@ -1,5 +1,5 @@
-export const AI_NAME = "Azure Chat";
-export const AI_DESCRIPTION = "Azure Chat is a friendly AI assistant.";
+export const AI_NAME = "Afnan GPT";
+export const AI_DESCRIPTION = "Afnan GPT is a friendly AI Chat assistant.";
 export const CHAT_DEFAULT_PERSONA = AI_NAME + " default";
 
 export const CHAT_DEFAULT_SYSTEM_PROMPT = `You are a friendly ${AI_NAME} AI assistant. You must always return in markdown format.
