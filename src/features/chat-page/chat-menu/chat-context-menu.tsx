e347a65c16dd5cafd36b0850321ab2a861889588
Jørgen Nesmoen <131775623+MallingForvaltning@users.noreply.github.com>
2025-08-12 12:11:12 +0200

    Revert "Simplify chat model selection and restore clear all option"

--- src/features/chat-page/chat-menu/chat-context-menu.tsx
+++ src/features/chat-page/chat-menu/chat-context-menu.tsx
@@ -17,7 +17,7 @@ export const ChatContextMenu = () => {
 
   const handleAction = async () => {
     if (
-      window.confirm("Er du sikker på at du vil tømme alle chattene?")
+      window.confirm("Are you sure you want to delete all the chat threads?")
     ) {
       setIsLoading(true);
       const response = await DeleteAllChatThreads();
@@ -43,7 +43,7 @@ export const ChatContextMenu = () => {
       <DropdownMenuContent side="right" align="start">
         <DropdownMenuItemWithIcon onClick={async () => await handleAction()}>
           <Trash size={18} />
-          <span>Tøm alle</span>
+          <span>Delete all</span>
         </DropdownMenuItemWithIcon>
       </DropdownMenuContent>
     </DropdownMenu>
